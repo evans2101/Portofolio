2c0b35671be77dd1464522ed18ea308d55654aa8
evans2101 <evans.moris21@gmail.com>
2023-07-27 15:47:39 +0700

    build animation

---
+++ src/assets/index.js
@@ -0,0 +1,108 @@
+import logo from "./logos.png";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+import creo from './company/creo.jpeg';
+import evoteks from './company/evoteks.png';
+import kalacoin from './company/kalacoin.png';
+import matakala from './company/matakala.jpg';
+import neuralink from './company/neuralink.jpeg';
+import nomina from './company/nomina.jpg';
+import polda from './company/polda.png';
+import sliva from './company/sliva.jpg';
+import slivalast from './company/slivalast.jpg';
+import w3 from './company/w3.png'
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+import swap from './swap.png';
+import farming from './farming.png';
+import Matakalaofficial from './matakala.png';
+import nftmatakala from './nftmatakala.png';
+import nusakala from './nusakalainstitute.png';
+import slivaacademy from './slivaacademy.png'
+import slivaindonesia from './slivaindonesia.png'
+import neuralinkweb from './neuralinkweb.png'
+import nominagames from './nominagames.png'
+import spn from './spn.png'
+import creoplay from './creoplay.png'
+import evermoreknights from './evermoreknights.png'
+
+export {
+  slivaacademy,
+  slivaindonesia,
+  neuralinkweb,
+  nominagames,
+  spn,
+  creoplay,
+  evermoreknights,
+  swap,
+  farming,
+  Matakalaofficial,
+  nftmatakala,
+  nusakala,
+  evoteks,
+  kalacoin,
+  matakala,
+  neuralink,
+  nomina,
+  polda,
+  sliva,
+  slivalast,
+  w3,
+  creo,
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,530 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  swap,
+  farming,
+  Matakalaofficial,
+  nftmatakala,
+  nusakala,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  evoteks,
+  kalacoin,
+  matakala,
+  neuralink,
+  nomina,
+  polda,
+  sliva,
+  slivalast,
+  w3,
+  creo,
+  slivaacademy,
+  slivaindonesia,
+  neuralinkweb,
+  nominagames,
+  spn,
+  creoplay,
+  evermoreknights,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Blockchain Developer",
+    icon: creator,
+  },
+  {
+    title: "Business Strategy Specialst",
+    icon: mobile,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "React.JS Developer",
+    company_name: "Personal Freelance",
+    icon: meta,
+    iconBg: "#383E56",
+    date: "2018 - Present",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Fullstack Developer",
+    company_name: "Matakala",
+    icon: matakala,
+    iconBg: "#383E56",
+    date: "2022",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Blockchain Engineer",
+    company_name: "Kala Coin",
+    icon: kalacoin,
+    iconBg: "#E6DEDD",
+    date: "2022",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Blockchain Engineer",
+    company_name: "Neuralink Protocol",
+    icon: neuralink,
+    iconBg: "#383E56",
+    date: "2022",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Fullstack Developer",
+    company_name: "Nomina Games",
+    icon: nomina,
+    iconBg: "#E6DEDD",
+    date: "2023 - Present",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Solidity Developer",
+    company_name: "Creo Engine",
+    icon: creo,
+    iconBg: "#E6DEDD",
+    date: "2022",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Web Developer",
+    company_name: "SPN POLDA RIAU",
+    icon: polda,
+    iconBg: "#E6DEDD",
+    date: "2022",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Fullstack Developer",
+    company_name: "Polda Riau",
+    icon: polda,
+    iconBg: "#E6DEDD",
+    date: "2022 - Present",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Founder & CEO",
+    company_name: "PT. Sliva Technology Indonesia",
+    icon: sliva,
+    iconBg: "#E6DEDD",
+    date: "2022 - Present",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Founder & CEO",
+    company_name: "SlivaAcademy",
+    icon: slivalast,
+    iconBg: "#E6DEDD",
+    date: "2022 - Present",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Founder & CEO",
+    company_name: "SlivaDoc",
+    icon: slivalast,
+    iconBg: "#E6DEDD",
+    date: "2023 - Present",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Coach React.JS",
+    company_name: "PT. Evolusi Teknologi Solusi",
+    icon: evoteks,
+    iconBg: "#E6DEDD",
+    date: "2023",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Coach Web3 | Blockchain",
+    company_name: "Web3 Weekend Asia",
+    icon: w3,
+    iconBg: "#E6DEDD",
+    date: "2023",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Matakala",
+    description:
+      "Matakala build new market mechanism for decentralized finance - Our platform product allow you to securely create, trade, and advertise digital asset on blockchain.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: Matakalaofficial,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "NFT Matakala",
+    description:
+      "If NFT Matakala is a specific project or concept that has emerged after my knowledge cutoff, I recommend conducting further research through up-to-date sources to understand its specific details and implications within the NFT ecosystem.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: nftmatakala,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Nusakala Institute",
+    description:
+      "MATAKALA is a blockchain-based Crypto Project. Born with community support. Established on 21 Sep 2021. And now in the process of relaunch and rebranding",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: nusakala,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Nomina Games",
+    description:
+      "Nomina Games has developed and published several titles across various platforms, including PC, consoles, and mobile devices. Some of their games may include elements of adventure, puzzle-solving, and storytelling, catering to a wide range of players.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "MUI",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: nominagames,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Evermore Knights",
+    description:
+      "Evermore Knights is one of the interesting money-making games in the Web3 ecosystem for you to play. In this blockchain-based game, you will choose various characters with the ability to mine crypto.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "MUI",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: evermoreknights,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "CreoPlay",
+    description:
+      "CreoPlay is a kind of platform that facilitates most on-chain activities, from marketplace transactions, staking, to gamepedia.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "MUI",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: creoplay,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Neuralink Protocol",
+    description:
+      "Neuralink can process more transactions making it faster, cheaper, and greener to execute smart contracts.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: neuralinkweb,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Sliva Academy",
+    description:
+      "Education v5.0 with the concept of bootcamp in all fields with expert class quality",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: slivaacademy,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "SPN Polda Riau",
+    description:
+      "Spn Polda Riau Proficient, Commendable, Law-abiding, and Superior",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: spn,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
